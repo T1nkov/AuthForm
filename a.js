43d9e99c46260bda9413c2f7abe43822f5a8cd7a
Dmitry Tinkov <mrfredyon@gmail.com>
2023-10-15 22:46:49 +0300

    14.10

--- a.js
+++ a.js
@@ -1,2 +1,10 @@
-const arr = [0,1,2,3]
-const m = 2
+const emailInput = document.querySelector("#email");
+const passInput = document.querySelector("#pass");
+const btn = document.querySelector("button");
+
+const user = `[
+    {"email":"mrfredyon@gmail.com","password":"Dimadima21" }
+]`;
+btn.addEventListener("click", () => {
+    
+});
